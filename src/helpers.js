712919fc51c02f60e2fc8b398b010f4cec96ea99
FujiHaruka <fujiharuka@realglobe.jp>
2018-12-16 14:33:35 +0900

    エラーメッセージを親切に

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,23 @@
 export function formatTestResult (results = []) {
+  console.log(results)
   const succeeded = results.every(({ ok }) => ok)
   const firstLine = (succeeded ? '<span style="color: blue">SUCCEEDED!</span>' : '<span style="color: red">FAILED!</span>')
 
-  const messages = results.map(({ ok, name }) => `${ok ? 'OK:' : 'ERROR:'} ${name}`)
+  const messages = results.map(({ ok, name, error }) => {
+    if (ok) {
+      return `OK: ${name}`
+    } else {
+      if (error && error.message) {
+        const message = error.message.split('\n')
+          .filter(Boolean)
+          .map(line => '  > ' + line)
+          .join('\n')
+        return `ERROR: ${name}` + '\n' + message
+      } else {
+        return `ERROR: ${name}`
+      }
+    }
+  })
 
   return firstLine + '\n\n' + messages.join('\n')
 }
